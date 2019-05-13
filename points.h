5acd39e6fb20754df77e259e5e6e91ebd9d6d281
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-05-13 17:43:25 -0500

    Add files via upload

---
+++ points.h
@@ -0,0 +1,25 @@
+#ifndef MATH_H
+#define MATH_H
+#include <math.h>
+#endif
+
+typedef struct {
+    float x;
+    float y;
+} Point;
+
+Point rotate_point(float cx, float cy, float theta, Point p) {
+/* credit to Cameron for helping with this
+https://stackoverflow.com/a/2259502
+*/
+    float s = sin(theta);
+    float c = cos(theta);
+    p.x -= cx;
+    p.y -= cy;
+
+    float rotated_x = p.x * c - p.y * s;
+    float rotated_y = p.x * s + p.y * c;
+    p.x = rotated_x + cx;
+    p.y = rotated_y + cy;
+    return p;
+}
---
+++ spinning_triangle.cpp
@@ -0,0 +1,94 @@
+// Compile with the following:
+// g++ spinning_triangle.cpp `pkg-config --cflags --libs sdl2`
+
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <math.h>
+#include "points.h"
+
+#define GREEN 0, 255, 70
+#define BLACK 0, 0, 0
+
+int main() {
+    SDL_Renderer *renderer;
+    SDL_Window *main_window;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Now I'm going to create a window, 600x600 pixels, centred in the screen
+    main_window = SDL_CreateWindow(
+        "Spinning Triangle - Coded in C++ by Dante Falzone",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        600,
+        600,
+        0
+    );
+
+    // and a renderer so we can draw things on that window
+    renderer = SDL_CreateRenderer(
+        main_window,
+        0,
+        0
+    );
+
+    // Make sure that the user is properly bewildered if something goes wrong
+    if (main_window == NULL || renderer == NULL) {
+        std::cout << "Error! Something went horribly wrong!\n";
+        std::cout << "Error: " << SDL_GetError() << '\n';
+        return 1;
+    }
+
+    // Now to make some fancy variables!
+    bool window_open = true;
+    SDL_Event event;
+    Point angle_0 {300, (400 - (150 * sqrt(3)))};
+    Point angle_1 {150, 400};
+    Point angle_2 {450, 400};
+
+    while (window_open) {
+        SDL_SetRenderDrawColor(renderer, BLACK, 0);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, GREEN, 255);
+
+        SDL_RenderDrawLine(
+            renderer,
+            angle_0.x,
+            angle_0.y,
+            angle_1.x,
+            angle_1.y
+        );
+        SDL_RenderDrawLine(
+            renderer,
+            angle_1.x,
+            angle_1.y,
+            angle_2.x,
+            angle_2.y
+        );
+        SDL_RenderDrawLine(
+            renderer,
+            angle_2.x,
+            angle_2.y,
+            angle_0.x,
+            angle_0.y
+        );
+
+        angle_0 = rotate_point(300, 300, 0.001, angle_0);
+        angle_1 = rotate_point(300, 300, 0.001, angle_1);
+        angle_2 = rotate_point(300, 300, 0.001, angle_2);
+
+        SDL_RenderPresent(renderer);
+
+        SDL_PollEvent(&event);
+        switch (event.type) {
+            case SDL_QUIT:
+                window_open = false;
+                break;
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(main_window);
+    SDL_Quit();
+    return 0;
+}
