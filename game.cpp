529f9bd272fefa80a13c133be607ed6eec645afb
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-05-12 19:08:50 -0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -6,9 +6,9 @@
 
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window;
-    window = SDL_CreateWindow("Dante's Game", 0, 0, 640, 480, 0);
-    if (window == NULL) {
+    SDL_Window *main_window;
+    main_window = SDL_CreateWindow("Dante's Game", 0, 0, 640, 480, 0);
+    if (main_window == NULL) {
         printf("Error! Unable to create window for some reason!\n");
         printf("Error: %s\n", SDL_GetError());
         return 1;
@@ -16,7 +16,7 @@ int main() {
 
 
     SDL_Delay(5000);
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow(main_window);
     SDL_Quit();
     return 0;
 }
