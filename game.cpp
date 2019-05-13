c7d6993a3fe4fa63b6b42fbbaffa780ff770618b
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-05-12 19:11:36 -0500

    Added some comments

--- game.cpp
+++ game.cpp
@@ -4,8 +4,12 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
+/* All this program does at the moment is create a black GUI window that goes away after 5 seconds. */
 int main() {
+    // We don't really need everything, but meh, this works for now
     SDL_Init(SDL_INIT_EVERYTHING);
+
+    // Ooh, spooky pointers!
     SDL_Window *main_window;
     main_window = SDL_CreateWindow("Dante's Game", 0, 0, 640, 480, 0);
     if (main_window == NULL) {
@@ -14,8 +18,9 @@ int main() {
         return 1;
     }
 
-
+    // We're going to keep our window open for a little bit
     SDL_Delay(5000);
+
     SDL_DestroyWindow(main_window);
     SDL_Quit();
     return 0;
