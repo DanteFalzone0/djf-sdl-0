1a11338069e06146e52c2bfe2bf2b99244d976aa
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-05-14 10:32:50 -0500

    Update cpoints.h

--- cpoints.h
+++ cpoints.h
@@ -41,3 +41,11 @@ float rotate_point_y(float cx,
     py = rotated_y + cy;
     return py;
 }
+
+/* I don't know if this is the most computationally
+efficient way to do point rotations, but it works
+well enough. I do plan to make some more involved stuff
+with SDL, possibly even a basic 3d graphics engine,
+so if I need to optimize my performance I'll do it
+then. I find that writing in pure C is nice, though
+of course it lacks a lot of fun features that C++ has. */
