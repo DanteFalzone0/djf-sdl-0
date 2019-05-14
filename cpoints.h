5294385c6507aa86a1857fb4c70e22202de32fb2
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-05-14 11:43:16 -0500

    Removed superfluous comments

--- cpoints.h
+++ cpoints.h
@@ -1,22 +1,17 @@
 /* This is a purely procedural version of 'points.hpp'.
-'points.hpp' is also pretty procedural, but it has structs
-which makes it at least slightly object-oriented. I wrote
-this because I couldn't get 'points.hpp' to work properly
-in C, even when it was named 'points.h'. */
+I wrote this because I couldn't get 'points.hpp' to work
+properly in C, even when it was named 'points.h'. */
 #ifndef MATH_H
 #define MATH_H
 #include <math.h>
 #endif
 
-/* no structs here I'm afraid */
-
+/* rotate an x-coordinate about an arbitrary point */
 float rotate_point_x(float cx,
                      float cy,
                      float theta,
                      float px,
                      float py) {
-    /* So I formatted my arguments weirdly. Whatcha gonna
-    do abouddit? */
     float s = sin(theta);
     float c = cos(theta);
     px -= cx;
@@ -27,6 +22,7 @@ float rotate_point_x(float cx,
     return px;
 }
 
+/* rotate a y-coordinate about an arbitrary point */
 float rotate_point_y(float cx,
                      float cy,
                      float theta,
@@ -41,11 +37,3 @@ float rotate_point_y(float cx,
     py = rotated_y + cy;
     return py;
 }
-
-/* I don't know if this is the most computationally
-efficient way to do point rotations, but it works
-well enough. I do plan to make some more involved stuff
-with SDL, possibly even a basic 3d graphics engine,
-so if I need to optimize my performance I'll do it
-then. I find that writing in pure C is nice, though
-of course it lacks a lot of fun features that C++ has. */
